beaa3d4b3036268b36e9aa968dc59dd78e44d605
Nirvan66 <nstp6666@gmail.com>
2020-02-19 17:40:45 -0700

    sample voting system

---
+++ helloWrld/index.js
@@ -0,0 +1,37 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"))
+var account;
+web3.eth.getAccounts().then((f) => {
+ account = f[0];
+})
+
+abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
+contract = new web3.eth.Contract(abi);
+contract.options.address = "0x1220217d2329B6056DE99C83edfCB02B617181b1";
+// update this contract address with your contract address
+
+candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+
+function voteForCandidate(candidate) {
+ candidateName = $("#candidate").val();
+ console.log(candidateName);
+ console.log(account);
+ contract.methods.voteForCandidate(web3.utils.asciiToHex(candidateName)).send({from: account}).then((f) => {
+  let div_id = candidates[candidateName];
+  contract.methods.totalVotesFor(web3.utils.asciiToHex(candidateName)).call().then((f) => {
+   $("#" + div_id).html(f);
+  })
+ })
+}
+
+$(document).ready(function() {
+ candidateNames = Object.keys(candidates);
+
+ for(var i=0; i<candidateNames.length; i++) {
+ let name = candidateNames[i];
+  
+ contract.methods.totalVotesFor(web3.utils.asciiToHex(name)).call().then((f) => {
+  $("#" + candidates[name]).html(f);
+ })
+ }
+});
