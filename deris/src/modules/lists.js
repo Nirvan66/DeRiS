dda518f3d16f05d7207a86e3ce02f6a8d95f333c
Zach <zmcgrath96@gmail.com>
2020-03-07 21:16:19 -0700

    got list of buttonlabelelements to work from driver page

---
+++ deris/src/modules/lists.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ButtonWithLabels } from './../modules/buttonInputs.js'
+
+/**
+ * Build a list of buttonWithLabels elements
+ * 
+ * @param {object} props    properties from caller. Has the attributes
+ *                          {
+ *                              elements:       list the following
+ *                                  {   
+ *                                      buttonLabel:    str,
+ *                                      labels:         list of strings 
+ *                                  }
+ *                              onClick:        callable
+ *                          }
+ * @returns {React DOM element}
+ */
+function ButtonWithLabelsList(props) {
+    return (
+        <div class="buttonWithLabelsListContainer">
+            { props.elements.map( el =>
+                    <ButtonWithLabels
+                        buttonLabel={el.buttonLabel}
+                        labels={el.labels}
+                        onClick={() => {alert('oh ive been touched')}}
+                    ></ButtonWithLabels>
+                )}
+        </div>
+    )
+}
+
+export {
+    ButtonWithLabelsList
+}
--- deris/src/pages/driverPage.js
+++ deris/src/pages/driverPage.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { ButtonWithLabels } from './../modules/buttonInputs.js'
+import { ButtonWithLabelsList } from './../modules/lists.js'
 
+/**
+ * The module for the driver page
+ * 
+ * @param {object}  props       Properties passed in from the App. Should have 
+ *                              onSubmit: function that takes payload 
+ *                                  {
+ *                                      center:         number, 
+ *                                      radius:         number,
+ *                                      jobInfo:        object
+ *                                  }
+ *                                  request type is either 'request' or 'cancel'
+ *                              show: boolean to render the page
+ * 
+ * @returns {React.Component}   The react component for the entire page
+ */
 class DriverPage extends React.Component {
     constructor (props) {
         super(props);
@@ -9,18 +24,52 @@ class DriverPage extends React.Component {
             radius: null,
             jobInfo: null,
         }
+
+        this.getRides = this.getRides.bind(this);
+    }
+    
+    // Getting rides from blockchain. Mock for now
+    getRides() {
+        return {
+            elements:[
+                {
+                    buttonLabel: 'Accept',
+                    labels: [
+                        '123 applewood road', '456 nutmeg avenue', '13'
+                    ]
+                },
+                {
+                    buttonLabel: 'Accept',
+                    labels: [
+                        '802 fisherman road', '111 colorado avenue', '10'
+                    ]
+                },
+                {
+                    buttonLabel: 'Accept',
+                    labels: [
+                        '910 lakeview blvd', '100 cold street', '29'
+                    ]
+                }, 
+                {
+                    buttonLabel: 'Accept',
+                    labels: [
+                        '992 montgomery street', '529 eisenhower blvd', '2'
+                    ]
+                }
+            ]
+
+        }
     }
 
     render() {
         if (!this.props.show){
             return (<div class="empty"></div>)
         }
+        const elements = this.getRides();
         return (
-            <ButtonWithLabels
-                buttonLabel='dumbButton'
-                labels={['fish', 'cow', 'dog']}
-                onClick={() => {alert('oooo my button')}}
-            ></ButtonWithLabels>
+            <ButtonWithLabelsList
+                elements={elements.elements}
+            ></ButtonWithLabelsList>
         )
     }
 }
